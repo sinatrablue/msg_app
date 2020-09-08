08d662cc1f8d82ce31968d003766bfc0960d8024
sinatrablue <48838914+sinatrablue@users.noreply.github.com>
2020-09-08 17:25:09 +0200

    First commit

---
+++ Client.java
@@ -0,0 +1,64 @@
+package msgapp;
+
+import java.net.*;
+
+//import jdk.internal.jline.internal.InputStreamReader;
+
+import java.io.*;
+
+public class Client {
+    private Socket socket = null;   // initialize socket
+    private DataInputStream input = null;     // initialize input data stream
+    private DataOutputStream output = null;    // initialize output data steam
+
+    // function for ip adress and port :
+    private Client(String adress, int port) {
+
+        // try to etablish a connection
+        try {
+            socket = new Socket(adress, port);
+            System.out.println("Connected to : " + adress + "  |  on port : " + port);
+            // taking inputs from the client :
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            // then send this to the socket :
+            output = new DataOutputStream(socket.getOutputStream());
+        }
+
+        // Exceptions management :
+        catch(UnknownHostException u_h) {
+            System.out.println(u_h);
+        }
+
+        catch(IOException i_o) {
+            System.out.println(i_o);
+        }
+
+        // Blank string to be filled with input :
+        String line = "";
+
+        // Read until end message is found
+        while(!line.equals("--Over")){
+            try {
+                line = input.readLine();    // to correct with buffers if better --> searches
+                output.writeUTF(line);
+            }
+            catch(IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        // Close the connection as communication is terminated :
+        try {
+            input.close();
+            output.close();
+            socket.close();
+        }
+        catch(IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        Client client = new Client("127.0.0.1", 5000);  // guessing that 127.0.0.1 is localhost --> verify + why port 5000 ?
+    }
+}
---
+++ Server.java
@@ -0,0 +1,50 @@
+package msgapp;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    // initialize sockets and stream :
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+
+    // constructor only using a port :
+    public Server(int port){
+        // starting server and waiting for a connection
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started, on port : " + port + " ; Waiting for a connection...");
+    
+            socket = server.accept();
+            System.out.println("Client accepted.");
+    
+            // taking inputs from the client socket :
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            String line = "";
+    
+            // reading from the client until end message
+            while(!line.equals("--Over")){
+                try {
+                    line = in.readUTF();
+                    System.out.println(line);
+                }
+                catch(IOException i){
+                    System.out.println(i);
+                }
+            }
+            // End message is read
+            System.out.println("Now closing connection...");
+            socket.close();
+            in.close();
+            System.out.println("Connection closed.");
+        }
+        catch(IOException i){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]){
+        Server server = new Server(5000);
+    }
+}
